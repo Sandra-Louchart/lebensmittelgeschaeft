891f7ed9cbe46667c995cae91a3ffce04ed4f69f
sandraLouchart <sandra.louchart@gmail.com>
2022-05-20 15:59:34 +0200

    init vue register

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,8 @@ Encore
      * and one CSS file (e.g. app.scss) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/js/app.js')
+    .addEntry('register', './assets/js/register.js')
+
     .addEntry('home', './assets/styles/home.scss')
 
 
