7f92da14e81d47a47d0038a7552cb9eae1112914
sandraLouchart <sandra.louchart@gmail.com>
2022-05-21 13:54:39 +0200

    CRUD + upload mit Vich

--- src/Entity/Products.php
+++ src/Entity/Products.php
@@ -3,9 +3,14 @@
 namespace App\Entity;
 
 use App\Repository\ProductsRepository;
+use DateTimeInterface;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
 
 #[ORM\Entity(repositoryClass: ProductsRepository::class)]
+#[Vich\Uploadable]
 class Products
 {
     #[ORM\Id]
@@ -22,6 +27,12 @@ class Products
     #[ORM\Column(type: 'string', length: 255)]
     private $picture;
 
+    #[Vich\UploadableField(mapping: 'products_pictures', fileNameProperty: 'pictureName')]
+    private ?File $pictureFile = null;
+
+    #[ORM\Column(type: 'string')]
+    private ?string $pictureName = null;
+
     #[ORM\Column(type: 'string', length: 255)]
     private $subtitle;
 
@@ -30,6 +41,9 @@ class Products
 
     #[ORM\Column(type: 'float')]
     private $price;
+
+    #[ORM\Column(type: 'datetime')]
+    public DateTimeInterface $updatedAt;
 
     #[ORM\ManyToOne(targetEntity: Category::class, inversedBy: 'products')]
     #[ORM\JoinColumn(nullable: false)]
@@ -74,6 +88,33 @@ class Products
         $this->picture = $picture;
 
         return $this;
+    }
+
+    /**
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $pictureFile
+     */
+    public function setpictureFile(?File $pictureFile = null): void
+    {
+        $this->pictureFile = $pictureFile;
+
+        if (null !== $pictureFile) {
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getpictureFile(): ?File
+    {
+        return $this->pictureFile;
+    }
+
+    public function setPictureName(?string $pictureName): void
+    {
+        $this->pictureName = $pictureName;
+    }
+
+    public function getPictureName(): ?string
+    {
+        return $this->pictureName;
     }
 
     public function getSubtitle(): ?string
