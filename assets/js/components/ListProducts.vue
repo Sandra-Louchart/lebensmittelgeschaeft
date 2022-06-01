c44f399476cc1e2148b582a2c78d1ea2d0869dbe
sandraLouchart <sandra.louchart@gmail.com>
2022-06-01 18:00:38 +0200

    admin admin

--- assets/js/components/ListProducts.vue
+++ assets/js/components/ListProducts.vue
@@ -1,18 +1,13 @@
 <template>
-  <section id="app" class="pt-5-pb-5 mt-5 mb-5">
-  <div  class="container">
-    <div class="row">
-      <div class="col-6">
-        <h2 class="text-primary text-bg-dark">Unsere Aktionen</h2>
-      </div>
-    <div class="col-12">
-      <div id="carouselExampleIndicators2" class="carousel slide" data-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <div class="row">
-              <div v-for="product in products" class="card text-center col-md-4 p-3">
-                <div class="card-header bg-primary text-dark">{{product.name}}</div>
-                <div class="card-body">
+  <section id="app" class="pt-5-pb-5 mb-5 mb-5">
+    <div  class="container">
+      <div class="row">
+        <div class="col-12">
+          <div class="row">
+            <div v-for="product in products" :key="product.id" class="col-md-6 pt-5 pb-2">
+              <div class="account-item text-center text-xxl-center">
+                <div class="card-header bg-secondary text-dark">{{product.name}}</div>
+                <div class="card-body bg-primary text-dark">
                   <h5 class="card-title">{{product.price}} €</h5>
                   <p class="card-text"> {{product.subtitle}}</p>
                 </div>
@@ -22,9 +17,8 @@
         </div>
       </div>
     </div>
-    </div>
-  </div>
-</section>
+  </section>
+
 </template>
 
 <script>
@@ -42,7 +36,6 @@ const axios = require('axios');
      this.products = results.data
      console.log(this.products)
    },
-
 
 };
 </script>
